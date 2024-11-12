bfaf6ab5f357103ca67cbcb6d9bfbca400c97362
David Martínez García <94689492+david-martinez-garcia@users.noreply.github.com>
2024-11-12 11:07:27 +0100

    Version 3.0.0: first release

--- data_product_manager/main.py
+++ data_product_manager/main.py
@@ -731,7 +731,12 @@ def create_alignment(translation_rules: bytes) -> dict:
         },
         data = translation_rules.decode("utf-8")
     )
-    # Handle response.
+    if response.status_code == status.HTTP_201_CREATED:
+        logger.info("Alignment " + response.json["info"]["name"] + "/" + response.json["info"]["version"] + " created successfully.")
+        return response.json["info"]
+    else:
+        logger.info("Exception while trying to create alignment: " + response.json["message"])
+        raise HTTPException(status_code = response.status_code, detail = response.json["message"])
 
 def delete_alignment(name: str, version: str) -> None:
     '''
@@ -783,7 +788,8 @@ def create_translation_channel(translation_channel_settings: dict, data_product:
     )
     if response.status_code == status.HTTP_201_CREATED:
         logger.info("Translation channel successfully created.")
-        translation_channel_settings.update({"channelId": str(response.json["channel"]["id"])})
+        translation_channel_settings.update({"channelId": str(response.json["info"]["id"])})
+        translation_channel_settings.update({"descId": str(response.json["info"]["descId"])})
         data_product["translation"]["settings"] = translation_channel_settings
     else:
         logger.info("Exception while trying to create a new translation channel.")
